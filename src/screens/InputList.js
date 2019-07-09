44b1793fc1cf2dff47447d99cf4b2f4daf5012cf
David Herrera <dacherreragu@unal.edu.co>
2019-07-08 22:01:48 -0500

    Crear lista (estilos

--- src/screens/InputList.js
+++ src/screens/InputList.js
@@ -112,27 +112,38 @@ class InputList extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-        <Picker
-          selectedValue={this.state.existPlace}
-          onValueChange={
-            this.changeExistenPlace
-          }
-        >
-          <Picker.Item label="Seleccione un lugar" value='-1' />
-          {this.state.pickItems}
-        </Picker>
+
+        <Text style={styles.welcome}>Crear lista de lugares</Text>
+
+        <Text style={styles.subTitle}>Seleccionar lugar existente</Text>
+        
+        <View style={styles.prePicker}>
+          <Picker
+            selectedValue={this.state.existPlace}
+            onValueChange={
+              this.changeExistenPlace
+            }
+            style={styles.picker}
+          >
+            <Picker.Item label="Seleccione un lugar" value='-1' 
+            style={styles.pickerItem}/>
+            {this.state.pickItems}
+          </Picker>
+        </View>
+
         <TouchableOpacity
           onPress={this.addPlaceExistenced}
+          style={styles.btnEnter}
         >
           <Text style={styles.btnEnterText}>Adicionar este lugar</Text>
         </TouchableOpacity>
 
-        <Text >Crear lugar</Text>
+        <Text style={styles.subTitle}>Crear lugar</Text>
 
         <View style={styles.outMaps}>
           <MapView
             provider={this.props.provider}
-            style={styles.map}
+            style={styles.map1}
             initialRegion={{
               latitude: LATITUDE,
               longitude: LONGITUDE,
@@ -176,32 +187,35 @@ class InputList extends React.Component {
           </TouchableOpacity>
         </View>
 
+        <Text style={styles.subTitle}>Lista de lugares</Text>
 
-        <Text style={styles.labelField}>Tu lista de lugares: </Text>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: LATITUDE,
-            longitude: LONGITUDE,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          }}
-        >
-          {this.state.pinsOfMap}
-        </MapView>
+        <View style={styles.outMaps}>
+          <MapView
+            style={styles.map2}
+            region={{
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+            }}
+          >
+            {this.state.pinsOfMap}
+          </MapView>
+        </View>
+
         <Text style={styles.labelField}>Nombre de la lista </Text>
         <TextInput
           style={styles.input}
-          placeholder="Ingrese un nombre creativo para que recuerdes!!"
+          placeholder="Ingrese el nombre de la lista"
           onChangeText={(nameList) => this.setState({ nameList: nameList })}
           value={this.state.nameList}
           autoCapitalize="none"
         />
 
-        <Text style={styles.labelField}>Commentarios</Text>
+        <Text style={styles.labelField}>Comentarios</Text>
         <TextInput
           style={styles.inputDescription}
-          placeholder="Notas pára recordar :)"
+          placeholder="Ingrese sus comentarios sobre la lista"
           onChangeText={(descriptionList) => this.setState({ descriptionList: descriptionList })}
           value={this.state.descriptionList}
           autoCapitalize="none"
@@ -366,9 +380,6 @@ class InputList extends React.Component {
       alert(response.data.createList.message)
 
     }
-
-
-
   }
 
   safePlacesInMS = async (idList, idPlace) => {
@@ -411,17 +422,65 @@ class InputList extends React.Component {
     }
   }
 
-
-
-
 }
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#2B303A',
     fontFamily: 'sans-serif',
   },
-  map: {
-    height: 300,
+  welcome: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#D64933',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  subTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#92DCE5',
+    textAlign: 'center',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  prePicker: {
+    borderRadius: 15,
+    backgroundColor: '#FFFFFF',
+    marginTop: 10,
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 15,
+    padding: 5,
+  },
+  picker: {
+    height: 35,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pickerItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 15,
+    padding: 5,
+    fontSize: 20,
+  },
+  map1: {
+    height: 250,
+  },
+  map2: {
+    height: 350,
   },
   labelField: {
     fontSize: 20,
@@ -454,11 +513,13 @@ const styles = StyleSheet.create({
     height: 300,
   },
   btnEnter: {
+    height: 45,
+    justifyContent: 'center',
     alignItems: 'center',
     marginTop: 10,
-    marginBottom: 30,
-    marginLeft: 40,
-    marginRight: 40,
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
     borderRadius: 15,
     padding: 5,
     backgroundColor: "#92DCE5",
@@ -466,8 +527,6 @@ const styles = StyleSheet.create({
   },
   btnEnterText: {
     fontSize: 20,
-    // color: '#2B303A',
-    paddingBottom: 20,
   },
   outMaps: {
     borderWidth: 4,
